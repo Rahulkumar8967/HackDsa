9ae75f1118ec05ba67d0860ce14f8030305cb826
RahulKumar8967 <sainirahul8967@gmail.com>
2024-03-07 10:14:27 +0530

    Add

--- striverseries/BasicHashing/countnumber.cpp
+++ striverseries/BasicHashing/countnumber.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(){
 int n;
+cout<<"Enter the size of the Array "<<" ";
 cin >> n;
+
+cout<<"Enter the elements"<<endl;
 int arr[n];
 for(int i = 0;i<n;i++){
     cin >> arr[i];
@@ -14,6 +17,7 @@ int hash[13] = {0};
 for(int i= 0;i<n;i++){
     hash[arr[i]] += 1;
 }
+cout<<"precompute done and & how many query you want !"<<endl;;
 int q;  // how many element they have search
 cin >> q;
 while(q--){
@@ -23,6 +27,6 @@ while(q--){
 // fetch
 cout<< hash[number] << endl;
 }
-
+cout<<"fetching is done";
     return 0;
 }
